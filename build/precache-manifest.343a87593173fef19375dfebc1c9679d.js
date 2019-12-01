bb0fed196a561a478c2192caa4e79388296b6160
Sufian Abu-Rab <s.aburab@gmail.com>
2019-12-01 23:45:49 +0000

    update frontpage

---
+++ build/precache-manifest.343a87593173fef19375dfebc1c9679d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78324bda655b936e503e7c4c1a3ca1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633d64c9ac61e585c442",
+    "url": "/static/css/main.a3aae21d.chunk.css"
+  },
+  {
+    "revision": "dcd9d778a1eba991a684",
+    "url": "/static/js/2.753128a3.chunk.js"
+  },
+  {
+    "revision": "633d64c9ac61e585c442",
+    "url": "/static/js/main.33d0350d.chunk.js"
+  },
+  {
+    "revision": "aceb0f5f4b6c3f5a6a84",
+    "url": "/static/js/runtime-main.3c91196d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -6,18 +6,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>Selfmade Events</div>
+          <form
+            action="https://www.paypal.com/cgi-bin/webscr"
+            method="post"
+            target="_top"
+          >
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input
+              type="hidden"
+              name="hosted_button_id"
+              value="FHMQ5GYCK9UPA"
+            />
+            <input
+              type="image"
+              src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donate_LG.gif"
+              border="0"
+              name="submit"
+              title="PayPal - The safer, easier way to pay online!"
+              alt="Spenden mit dem PayPal-Button"
+            />
+            <img
+              alt=""
+              border="0"
+              src="https://www.paypal.com/de_DE/i/scr/pixel.gif"
+              width="1"
+              height="1"
+            />
+          </form>
       </header>
     </div>
   );
